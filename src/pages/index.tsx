1f1979250c617cec4a10dc6f9d9328e632aeac5a
jwyce <wycejared@gmail.com>
2022-03-09 18:52:59 -0500

    💄 Add default page styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-	return <div className="text-2xl text-red-800">hello world</div>;
+	return <div className="text-2xl text-red-300">hello world</div>;
 };
 
 export default Home;
