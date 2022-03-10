2ad05c69b7026c481f1af6e5bb4df14b0391ef73
jwyce <wycejared@gmail.com>
2022-03-09 19:55:29 -0500

    ✨ Add random pet id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,17 @@
-import { trpc } from '@/utils/trpc';
+import { getOptionsForVote } from '@/utils/getRandomPet';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-	const { data, status } = trpc.useQuery(['hello', { text: 'Jared' }]);
-
-	if (status === 'loading') {
-		return <p>Loading...</p>;
-	}
-
-	if (data) return <div>{data.greeting}</div>;
+	const [first, second] = getOptionsForVote();
 
 	return (
 		<div className="h-screen w-screen flex flex-col justify-center items-center">
 			<div className="text-2xl text-center">Which Pet do you like more?</div>
 			<div className="p-2"></div>
 			<div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-				<div className="w-16 h-16 bg-red-200"></div>
+				<div className="w-16 h-16 bg-red-700">{first}</div>
 				<div className="p-8">Vs</div>
-				<div className="w-16 h-16 bg-red-200"></div>
+				<div className="w-16 h-16 bg-red-700">{second}</div>
 			</div>
 		</div>
 	);
---
+++ src/utils/getRandomPet.ts
@@ -0,0 +1,14 @@
+const MAX_PET_ID = 89;
+
+export const getRandomPet = (notThisOne?: number): number => {
+	const petNumber = Math.floor(Math.random() * MAX_PET_ID) + 1;
+	if (petNumber !== notThisOne) return petNumber;
+	return getRandomPet(notThisOne);
+};
+
+export const getOptionsForVote = () => {
+	const firstId = getRandomPet();
+	const secondId = getRandomPet(firstId);
+
+	return [firstId, secondId];
+};
